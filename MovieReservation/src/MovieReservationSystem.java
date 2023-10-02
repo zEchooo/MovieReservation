5dd4354b7e1762cebf0f2338399fd22334ed28d4
IraTabora <iramonroetabora@yahoo.com>
2023-10-02 15:45:21 +0800

    java project commit

--- MovieReservation/src/MovieReservationSystem.java
+++ MovieReservation/src/MovieReservationSystem.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class MovieReservationSystem {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     Scanner scanner = new Scanner(System.in);
+     System.out.println("Good Day our fellow customers! Welcome to AWS Cinemas.");
+     System.out.println("Please choose whithin the 3 options: \n 1. Display Movie Lists \n 2. Display Available Seats \n 3. Reserve Seats");
+     String input = scanner.nextLine();
+
+     switch(input){
+        case "1":
+            MovieSchedule MovieScheduleInstance = new MovieSchedule();
+            MovieScheduleInstance.CSVReader();
+            break;
+            // Add cases for 2 and 3 here
+           
+     }
+
     }
 }
