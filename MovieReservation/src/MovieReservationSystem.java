9d4b727c9d3535a5f3446daba1ea69e865db8093
AWS-Kier-Magday <kier.magday@awsys-i.com>
2023-10-03 08:37:55 +0800

    first commit

--- MovieReservation/src/MovieReservationSystem.java
+++ MovieReservation/src/MovieReservationSystem.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class MovieReservationSystem {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MovieSchedule movieSchedule = new MovieSchedule("C:\\Users\\kmagday\\Downloads\\Test.csv");
+
+        // Load movies from a CSV file using CSVHandler
+
+        while (true) {
+            System.out.println("1. Reserve Seats");
+            System.out.println("2. Cancel Reservation");
+            System.out.print("Select an option: ");
+
+            int option = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (option) {
+                case 1:
+                try {
+                    System.out.println("Input date: ");
+                    String dateToReserve = scanner.next();
+                    movieSchedule.displayMovieSchedule(dateToReserve);
+
+                    System.out.println("Select movie: ");
+                    int choice = scanner.nextInt();
+                    Movie selectedMovie = movieSchedule.getMovieByIndex(dateToReserve,choice);
+                    
+                    System.out.println(selectedMovie.toString());
+                } catch (Exception e) {
+                    // TODO: handle exception
+                }
+                    break;
+                case 2:
+                    // Cancel Reservation
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
     }
 }
